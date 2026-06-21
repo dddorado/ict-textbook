ec509b593f21fc94d026e0e5ea010cec98b843ed
Dan Anthony Dorado <30865580+dddorado@users.noreply.github.com>
2026-06-22 00:05:01 +0800

    fix tables, figures, and download options

--- clean-unnumbered-title.lua
+++ clean-unnumbered-title.lua
@@ -12,3 +12,23 @@ function Meta(meta)
     return meta
   end
 end
+
+function Image(image)
+  if not FORMAT:match("latex") then
+    return image
+  end
+
+  local _, contents = pandoc.mediabag.fetch(image.src)
+  if contents and contents:sub(1, 8) == "\137PNG\r\n\26\n" then
+    local width = string.unpack(">I4", contents, 17)
+    local height = string.unpack(">I4", contents, 21)
+    local max_width = 6.15
+    local max_height = 6.40
+    local fitted_width = math.min(max_width, max_height * width / height)
+
+    image.attributes.width = string.format("%.3fin", fitted_width)
+    image.attributes.height = nil
+  end
+
+  return image
+end
